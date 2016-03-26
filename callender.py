928cf4924ba97f418dfeee1215e38e6cfcfa982c
shridarpatil <shridharpatil2792@gmail.com>
2016-03-26 11:31:55 +0530

    first commit

---
+++ callender.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import calendar
+
+# ask of month and year
+yy = int(input("Enter year: "))
+mm = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(yy,mm))
